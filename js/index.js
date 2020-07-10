82894855d453acf9fd26595f281ff887df9640bb
Richard Burner <60147649+RichB22@users.noreply.github.com>
2020-07-10 15:40:12 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
-
+//import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
+
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
